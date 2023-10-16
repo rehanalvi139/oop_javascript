f065ac3dc2b2ef03febe83d8fcfee1fe7dccddff
rehanalvi123 <rehanghq@gmail.com>
2023-10-16 11:54:03 +0500

    first commit

---
+++ getobjects.js
@@ -0,0 +1,8 @@
+var userone = {
+    email: 'rehanghq@gmail.com',
+    mobile: '03469200752',
+    login() { this.email, 'User login' },
+    logout() { this.email, 'User logout' },
+};
+
+console.log(userone.email);
